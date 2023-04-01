b20e6351b708f531f6674ca9dd6bcbe0a1ee644f
AndreyDK13 <Aspirant_Kravtsov@mail.ru>
2023-04-01 13:16:50 +0300

    Added lections 1, 3

---
+++ Знакомство с языком Python (лекции)/lesson_1/main.py
@@ -0,0 +1,12 @@
+# a = 5
+# b = 5.89
+# c = 'hello'
+
+# print(f"{a},{b},{c}")
+# print("{} -- {} -- {}".format(a,b,c))
+
+# print('Input first value: ')
+# a = input()
+# print(a)
+
+a = input('Input first value: ')
---
+++ Знакомство с языком Python (лекции)/lesson_3/main.py
@@ -0,0 +1,31 @@
+# sum function
+
+# def summ (n):
+#     sum = 0
+#     for i in range(1, n+1):
+#         sum += i
+#     return sum
+
+# a = int (input ('Input number'))
+# print (summ (a))
+
+# 
+
+# def concat (*args):
+#     res = ''
+#     for i in args:
+#         res += i
+#     return res
+
+# # print (concat ('a', 'b', 'c', 'v'))
+# print (concat (1,5,9))
+
+# fibonacci sequence
+
+def fib (n):
+    if n in (1,2)
+        return 1
+    return fib(n-1) + fib (n-2)
+
+print (fib(10))
+
